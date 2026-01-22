6774da0fc9a7d3d17a8fc6e73f910c43fd9dbe00
Teja <tejad1819@gmail.com>
2026-01-22 14:47:56 -0600

    did some chnages in email sender for dev failure

--- src/ApplicationCore/Interfaces/IEmailSender.cs
+++ src/ApplicationCore/Interfaces/IEmailSender.cs
@@ -3,6 +3,9 @@
 namespace Microsoft.eShopWeb.ApplicationCore.Interfaces;
 
 public interface IEmailSender
+
 {
+    throw new Exception("DEV runtime failure test");
+
     Task SendEmailAsync(string email, string subject, string message);
 }
