7f55b7152bb523ac9caea3c6935ba55719a79b37
Teja <tejad1819@gmail.com>
2026-01-22 15:06:19 -0600

    did changes in user controller for dev failure

--- src/ApplicationCore/Interfaces/IEmailSender.cs
+++ src/ApplicationCore/Interfaces/IEmailSender.cs
@@ -5,7 +5,6 @@ namespace Microsoft.eShopWeb.ApplicationCore.Interfaces;
 public interface IEmailSender
 
 {
-    throw new Exception("DEV runtime failure test");
 
     Task SendEmailAsync(string email, string subject, string message);
 }
